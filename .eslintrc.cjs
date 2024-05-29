5905ee84c9515b84bd2ae5c8df53be46d65b4177
YutoFuse <yuto7696@gmail.com>
2024-05-29 16:52:18 +0900

    feat: eslint, prettierの設定

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,36 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended', 
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react', 'prettier'], 
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off', 
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off',
+    'react/no-unknown-property': ['warn', { ignore: ['css'] }],
+    'no-var': 'error',
+    'no-console': 'warn',
+    camelcase: 'warn',
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
