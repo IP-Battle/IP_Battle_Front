4c9dcdbd895228ab527936fe89fd0e8f91aa337b
YutoFuse <yuto7696@gmail.com>
2024-05-29 17:15:37 +0900

    fix: vite.config.ts修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    // ↓追記
-    resolve: {
-        alias: {
-            "@": "/src",
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-});
+  },
+})
