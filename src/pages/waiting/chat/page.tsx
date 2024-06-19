766194ba442290c843c0f283efc738929add658e
YutoFuse <yuto7696@gmail.com>
2024-06-19 09:22:26 +0900

    feat: サイドメニュー作成

--- src/pages/waiting/chat/page.tsx
+++ src/pages/waiting/chat/page.tsx
@@ -1,1 +1,13 @@
 // TODO: チャット画面作成
+import SideMenu from '../sideMenu/sideMenu'
+import { Typography } from '@material-tailwind/react'
+
+function ChatPage() {
+  return (
+    <SideMenu>
+      <Typography>チャット欄</Typography>
+    </SideMenu>
+  )
+}
+
+export default ChatPage
--- src/pages/waiting/probLog/page.tsx
+++ src/pages/waiting/probLog/page.tsx
@@ -1,1 +1,13 @@
 // TODO: 問題履歴画面作成
+import SideMenu from '../sideMenu/sideMenu'
+import { Typography } from '@material-tailwind/react'
+
+function ProbLogPage() {
+  return (
+    <SideMenu>
+      <Typography>問題履歴</Typography>
+    </SideMenu>
+  )
+}
+
+export default ProbLogPage
