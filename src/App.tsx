b498f5b0d87c3bc6cbf7a531ddf79b7514fe5fc4
YutoFuse <yuto7696@gmail.com>
2024-06-19 22:18:18 +0900

    feat: ソケット追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,58 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ConnectionManager } from './components/socket/ConnectionManager'
+import { ConnectionState } from './components/socket/ConnectionState'
+import { Events } from './components/socket/Events'
+import { ROUTES } from './constants/route'
 import RootPage from './pages/page'
-import WaitingPage from './pages/waiting/page'
-import ProbLogPage from './pages/waiting/probLog/page'
 import ChatPage from './pages/waiting/chat/page'
 import NotePage from './pages/waiting/note/page'
-import { ROUTES } from './constants/route'
+import WaitingPage from './pages/waiting/page'
+import ProbLogPage from './pages/waiting/probLog/page'
+import { socket } from './socket'
 
 const App = () => {
+  const [isConnected, setIsConnected] = useState(socket.connected)
+  const [fooEvents, setFooEvents] = useState<string[]>([])
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true)
+    }
+    function onDisconnect() {
+      setIsConnected(false)
+    }
+    function onFooEvent(value: string) {
+      setFooEvents((prev) => [...prev, value])
+    }
+    socket.on('connect', onConnect)
+    socket.on('disconnect', onDisconnect)
+    socket.on('foo', onFooEvent)
+
+    return () => {
+      socket.off('connect', onConnect)
+      socket.off('disconnect', onDisconnect)
+      socket.off('foo', onFooEvent)
+    }
+  }, [])
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={ROUTES.ROOT} element={<RootPage />} />
-        <Route path={ROUTES.WAITING} element={<WaitingPage />}></Route>
-        <Route path={ROUTES.WAITING_CHAT} element={<ChatPage />}></Route>
-        <Route path={ROUTES.WAITING_PROBLOG} element={<ProbLogPage />}></Route>
-        <Route path={ROUTES.WAITING_NOTE} element={<NotePage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div className='App'>
+      <div className='hidden'>
+        <ConnectionState isConnected={isConnected} />
+        <Events events={fooEvents} />
+        <ConnectionManager />
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={ROUTES.ROOT} element={<RootPage />} />
+          <Route path={ROUTES.WAITING} element={<WaitingPage />}></Route>
+          <Route path={ROUTES.WAITING_CHAT} element={<ChatPage />}></Route>
+          <Route path={ROUTES.WAITING_PROBLOG} element={<ProbLogPage />}></Route>
+          <Route path={ROUTES.WAITING_NOTE} element={<NotePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
