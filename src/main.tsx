5656732376ad33d44ea2ce517c6be870523cbba8
YutoFuse <yuto7696@gmail.com>
2024-07-03 17:49:14 +0900

    fix: ソケット通信周りの修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { ThemeProvider } from '@material-tailwind/react'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
@@ -8,10 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root')!)
 
 root.render(
   <React.StrictMode>
-    {/* <ThemeProvider> */}
-    <div className='p-4 h-screen'>
+    <BrowserRouter>
       <App />
-    </div>
-    {/* </ThemeProvider> */}
+    </BrowserRouter>
   </React.StrictMode>
 )
--- src/pages/matching/page.tsx
+++ src/pages/matching/page.tsx
@@ -1,23 +1,8 @@
-import useGoToPage from '@/components/method/GoToPage/goToPage'
-import { ROUTES } from '@/constants/route'
-import { useEffect } from 'react'
+import { socket } from '@/socket'
 
 const MatchingPage = () => {
-  const goToPage = useGoToPage()
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      goToPage(ROUTES.MATCHING_PLAY)
-    }, 3000)
-
-    return () => clearTimeout(timer)
-  }, [goToPage])
-
-  return (
-    <div className='items-center justify-center flex h-screen'>
-      <p>matching...</p>
-    </div>
-  )
+  socket.emit('matching')
+  return <div className='items-center justify-center flex h-screen'>matching</div>
 }
 
 export default MatchingPage
