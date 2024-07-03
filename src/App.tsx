ab52d63dca0194b07a4354cf4f5444fcfba13dd9
YutoFuse <yuto7696@gmail.com>
2024-07-03 17:49:14 +0900

    fix: ソケット通信周りの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Route, Routes } from 'react-router-dom'
+// import { Events } from './components/socket/Events'
+import { Button } from '@material-tailwind/react'
 import { useEffect, useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import useGoToPage from './components/method/GoToPage/goToPage'
 import { ConnectionManager } from './components/socket/ConnectionManager'
 import { ConnectionState } from './components/socket/ConnectionState'
-import { Events } from './components/socket/Events'
 import { ROUTES } from './constants/route'
 import ConfigPage from './pages/config/page'
 import MatchingPage from './pages/matching/page'
@@ -14,9 +17,24 @@ import WaitingPage from './pages/waiting/page'
 import ProbLogPage from './pages/waiting/probLog/page'
 import { socket } from './socket'
 
+export type questionType = {
+  id: number
+  questionText: string
+  aSelect: string
+  iSelect: string
+  uSelect: string
+  eSelect: string
+  answer: string
+  explanation: string
+  accuracyRate: number
+}
+
 const App = () => {
+  const goToPage = useGoToPage()
   const [isConnected, setIsConnected] = useState(socket.connected)
-  const [fooEvents, setFooEvents] = useState<string[]>([])
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [playerId, setPlayerId] = useState<string | undefined>()
+  const [question, setQuestion] = useState<string | undefined>()
 
   useEffect(() => {
     const onConnect = () => {
@@ -25,40 +43,50 @@ const App = () => {
     const onDisconnect = () => {
       setIsConnected(false)
     }
-    const onFooEvent = (value: string) => {
-      setFooEvents((prev) => [...prev, value])
-      console.log(value)
+    const onJoinPlayerEvent = (playerid: string) => {
+      setPlayerId(playerid)
+    }
+    const onResponseQuestion = (question: questionType) => {
+      console.log(question)
+    }
+    const onGameStart = () => {
+      goToPage(ROUTES.MATCHING_PLAY)
     }
     socket.on('connect', onConnect)
     socket.on('disconnect', onDisconnect)
-    socket.on('foo', onFooEvent)
+    socket.on('joinPlayer', onJoinPlayerEvent)
+    socket.on('responseQuestion', onResponseQuestion)
+    socket.on('gameStart', onGameStart)
 
     return () => {
       socket.off('connect', onConnect)
       socket.off('disconnect', onDisconnect)
-      socket.off('foo', onFooEvent)
+      socket.off('joinPlayer', onJoinPlayerEvent)
+      socket.off('responseQuestion', onResponseQuestion)
+      socket.off('gameStart', onGameStart)
     }
-  }, [])
+  }, [goToPage])
 
   return (
     <div className='App'>
       <div className='hidden'>
         <ConnectionState isConnected={isConnected} />
-        <Events events={fooEvents} />
+        {/* <Events events={playerId} /> */}
         <ConnectionManager />
       </div>
-      <BrowserRouter>
-        <Routes>
-          <Route path={ROUTES.ROOT} element={<RootPage />} />
-          <Route path={ROUTES.WAITING} element={<WaitingPage />}></Route>
-          <Route path={ROUTES.WAITING_CHAT} element={<ChatPage />}></Route>
-          <Route path={ROUTES.WAITING_PROBLOG} element={<ProbLogPage />}></Route>
-          <Route path={ROUTES.WAITING_NOTE} element={<NotePage />}></Route>
-          <Route path={ROUTES.MATCHING} element={<MatchingPage />}></Route>
-          <Route path={ROUTES.MATCHING_PLAY} element={<PlayPage />}></Route>
-          <Route path={ROUTES.CONFIG} element={<ConfigPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Button className='hidden' color='blue' onClick={() => socket.emit('feachQuestion', [2, 3])}>
+        click
+      </Button>
+      <Routes>
+        <Route path={ROUTES.ROOT} element={<RootPage />} />
+        <Route path={ROUTES.WAITING} element={<WaitingPage />}></Route>
+        <Route path={ROUTES.WAITING_CHAT} element={<ChatPage />}></Route>
+        <Route path={ROUTES.WAITING_PROBLOG} element={<ProbLogPage />}></Route>
+        <Route path={ROUTES.WAITING_NOTE} element={<NotePage />}></Route>
+        <Route path={ROUTES.MATCHING} element={<MatchingPage />}></Route>
+        <Route path={ROUTES.MATCHING_PLAY} element={<PlayPage />}></Route>
+        <Route path={ROUTES.CONFIG} element={<ConfigPage />}></Route>
+      </Routes>
     </div>
   )
 }
