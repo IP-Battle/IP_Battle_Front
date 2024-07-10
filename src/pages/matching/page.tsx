b1313883d8410a6aa4b856ad3e5d7ec78add26ba
YutoFuse <yuto7696@gmail.com>
2024-07-10 19:33:48 +0900

    feat: プレイ画面機能設計

--- src/pages/matching/page.tsx
+++ src/pages/matching/page.tsx
@@ -1,7 +1,23 @@
+import useGoToPage from '@/components/method/GoToPage/goToPage'
+import { ROUTES } from '@/constants/route'
 import { socket } from '@/socket'
+import { useEffect } from 'react'
 
 const MatchingPage = () => {
   socket.emit('matching')
+
+  const goToPage = useGoToPage()
+
+  useEffect(() => {
+    const onGameStart = () => {
+      goToPage(ROUTES.MATCHING_PLAY)
+    }
+    socket.on('gameStart', onGameStart)
+
+    return () => {
+      socket.off('gameStart', onGameStart)
+    }
+  }, [goToPage])
   return <div className='items-center justify-center flex h-screen'>matching</div>
 }
 
