d4612f8fc5a601bfaf43d7b8274e949e5b8cec97
YutoFuse <yuto7696@gmail.com>
2024-07-15 23:23:45 +0900

    fix: roomId取得変更

--- src/pages/matching/page.tsx
+++ src/pages/matching/page.tsx
@@ -1,29 +1,28 @@
 import useGoToPage from '@/components/method/GoToPage/goToPage'
 import { ROUTES } from '@/constants/route'
+import { useRoom } from '@/contexts/RoomContext'
 import { socket } from '@/socket'
-import { useEffect, useState } from 'react'
-
-export interface RoomIdProps {
-  roomId: number
-}
+import { useEffect } from 'react'
 
 const MatchingPage = () => {
-  socket.emit('matching')
-
+  const { setRoomId } = useRoom()
   const goToPage = useGoToPage()
-  const [roomId, setRoomId] = useState<number>()
 
   useEffect(() => {
+    socket.emit('matching')
+
     const onGameStart = (roomId: number) => {
       setRoomId(roomId)
       goToPage(ROUTES.MATCHING_PLAY)
     }
+
     socket.on('gameStart', onGameStart)
 
     return () => {
       socket.off('gameStart', onGameStart)
     }
-  }, [goToPage])
+  }, [goToPage, setRoomId])
+
   return <div className='items-center justify-center flex h-screen'>matching</div>
 }
 
