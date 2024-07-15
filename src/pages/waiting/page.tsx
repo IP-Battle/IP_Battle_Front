1c67fc9377bbc188e919fe7f3c8e4032ed4446ac
sanae <backup.ver.0713@gmail.com>
2024-07-16 04:18:52 +0900

    キャラクター挿入

--- src/pages/waiting/page.tsx
+++ src/pages/waiting/page.tsx
@@ -1,10 +1,19 @@
+import Female from '../../images/Female.png'
+import Male from '../../images/Male.png'
 import SideMenu from './sideMenu/sideMenu'
 
 function WaitingPage() {
   return (
     <div>
       <SideMenu>
-        <p className='flex items-center justify-center'>(キャラクター)</p>
+        <div className='flex items-center justify-center'>
+          <div className='hidden lg:block lg:w-1/2'>
+            <img src={Male} style={{ marginRight: '-50px' }} />
+          </div>
+          <div className='hidden lg:block lg:w-1/2'>
+            <img src={Female} style={{ marginLeft: '-50px' }} />
+          </div>
+        </div>
       </SideMenu>
     </div>
   )
